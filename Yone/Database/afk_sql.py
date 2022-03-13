1f5c3c75e903cc34d435b26635c688b3ed883505
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-13 20:48:04 +0530

    Update afk_sql.py

--- Yone/Database/afk_sql.py
+++ Yone/Database/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, BigInteger
 
-from Yone.Database.sql import BASE, SESSION
+from Yone.Database import BASE, SESSION
 
 
 class AFK(BASE):
