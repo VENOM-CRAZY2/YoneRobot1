9f9fa54d94c348d523ef9e350ef08ea2df269764
Ashu Op <97359676+VENOM-CRAZY@users.noreply.github.com>
2022-03-13 20:20:23 +0530

    Update __main__.py

--- Yone/__main__.py
+++ Yone/__main__.py
@@ -240,8 +240,8 @@ def yone_about_callback(update: Update, context: CallbackContext):
             reply_markup=InlineKeyboardMarkup(
                 [
                    [
-                     InlineKeyboardButton(text="Support", url="t.me/Yone_Support"),
-                     InlineKeyboardButton(text="News", url="t.me/Yone_Updates"),
+                     InlineKeyboardButton(text="Support", url="t.me/v3nom_support"),
+                     InlineKeyboardButton(text="News", url="t.me/gujiya_garam"),
                    ],
                    [
                     InlineKeyboardButton(text="Back", callback_data="yone_back")
