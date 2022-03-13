aff34c0c28bb99e2114c39e7744d6307c12a606f
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-13 21:02:55 +0530

    Create Chatbot.py

---
+++ Yone/Plugins/User/Chatbot.py
@@ -0,0 +1,101 @@
+import json
+import re
+import os
+import html
+import requests
+import Tianabot.modules.sql.kuki_sql as sql
+
+from time import sleep
+from telegram import ParseMode
+from Tianabot import dispatcher, updater, SUPPORT_CHAT
+from Tianabot.modules.log_channel import gloggable
+from telegram import (CallbackQuery, Chat, MessageEntity, InlineKeyboardButton,
+                      InlineKeyboardMarkup, Message, ParseMode, Update, Bot, User)
+
+from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
+                          DispatcherHandlerStop, Filters, MessageHandler,
+                          run_async)
+
+from telegram.error import BadRequest, RetryAfter, Unauthorized
+
+from Yone.modules.helper_funcs.filters import CustomFilters
+from Yone.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply 
+from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
+
+ 
+
+
+
+
+def kuki_message(context: CallbackContext, message):
+    reply_message = message.reply_to_message
+    if message.text.lower() == "kuki":
+        return True
+    if reply_message:
+        if reply_message.from_user.id == context.bot.get_me().id:
+            return True
+    else:
+        return False
+        
+
+def chatbot(update: Update, context: CallbackContext):
+    message = update.effective_message
+    chat_id = update.effective_chat.id
+    bot = context.bot
+    is_kuki = sql.is_kuki(chat_id)
+	
+    if message.text and not message.document:
+        if not kuki_message(context, message):
+            return
+        Message = message.text
+        bot.send_chat_action(chat_id, action="typing")
+        kukiurl = requests.get('https://www.kukiapi.xyz/api/apikey=KUKIg76Fg4EIo/botname/owner/message='+Message)
+        Kuki = json.loads(kukiurl.text)
+        kuki = Kuki['reply']
+        sleep(0.3)
+        message.reply_text(kuki, timeout=60)
+
+def list_all_chats(update: Update, context: CallbackContext):
+    chats = sql.get_all_kuki_chats()
+    text = "<b>KUKI-Enabled Chats</b>\n"
+    for chat in chats:
+        try:
+            x = context.bot.get_chat(int(*chat))
+            name = x.title or x.first_name
+            text += f"• <code>{name}</code>\n"
+        except (BadRequest, Unauthorized):
+            sql.rem_kuki(*chat)
+        except RetryAfter as e:
+            sleep(e.retry_after)
+    update.effective_message.reply_text(text, parse_mode="HTML")
+
+__help__ = """
+Chatbot utilizes the Tiana api which allows tiana to talk and provide a more interactive group chat experience.
+
+*Admins only Commands*:
+/chatbot - Shows chatbot control panel
+
+"""
+
+__mod_name__ = "CʜᴀᴛBᴏᴛ"
+
+
+CHATBOT_HANDLER = MessageHandler(
+    Filters.text & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!")
+                    & ~Filters.regex(r"^\/")), chatbot)
+LIST_ALL_CHATS_HANDLER = CommandHandler(
+    "allchats", list_all_chats, filters=CustomFilters.dev_filter)
+
+dispatcher.add_handler(ADD_CHAT_HANDLER)
+dispatcher.add_handler(CHATBOTK_HANDLER)
+dispatcher.add_handler(RM_CHAT_HANDLER)
+dispatcher.add_handler(LIST_ALL_CHATS_HANDLER)
+dispatcher.add_handler(CHATBOT_HANDLER)
+
+__handlers__ = [
+    ADD_CHAT_HANDLER,
+    CHATBOTK_HANDLER,
+    RM_CHAT_HANDLER,
+    LIST_ALL_CHATS_HANDLER,
+    CHATBOT_HANDLER,
+]
